063b4f097ee5f66ba85e586f8ae4d419d2613a85
FelipeVieira9 <felipe.vieiraa52@gmail.com>
2024-09-18 09:16:48 -0300

    Estrutura do site adicionada

--- frontend/public/script.js
+++ frontend/public/script.js
@@ -1,1 +1,27 @@
-console.log("Working");
+const input_func = document.getElementById('input_func');
+const input_deriv = document.getElementById('input_deriv');
+
+const drawGraphic = () => {
+    let width = document.getElementById('container_graphic').clientWidth;
+    let height = document.getElementById('container_graphic').clientHeight;
+    functionPlot({
+        title: "Gráfico",
+        target: "#graphic",
+        width,
+        height,
+        yAxis: { domain: [-1, 9] },
+        grid: true,
+        data: [
+          {
+            fn: input_func.value,
+            derivative: {
+              fn: input_deriv.value,
+              updateOnMouseMove: true
+            }
+          }
+        ]
+      });
+
+}
+
+input_deriv.addEventListener('change', () => drawGraphic());
